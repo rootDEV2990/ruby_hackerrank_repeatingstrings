9617b6dddaa398ada6bd518d01db78cce30aee94
Miguel Angel Enciso Sanchez <website.dev@icloud.com>
2020-08-18 10:20:08 -0500

    Testing new kinds of loops

--- repeatingstrings.rb
+++ repeatingstrings.rb
@@ -1,20 +1,22 @@
 def repeatedString(s, n)
-    if s.length > 1
-        count = 0
-        str = ""
-        remainder = n % (s.length)
-        cycles = (n - remainder) / s.length
-        while cycles > count
-            str = str + s
+   count = 0
+    str = ''
+    if s.length == 1
+        str = n
+    else
+        p s.split(//)
+        remainder = n % s.length
+        extras = n - remainder
+        loop do 
+        for item in s.split(//) do 
+            str << item
             count += 1
         end
-        others = s[(remainder - 1)]
-        str << others if s.length > 1
-        s = str.count("a")
-    else
-        s = n
+        break if count == extras
+        end
+        others = str << s[0..(remainder - 1)] if s.length > 1
+        str.count("a")
     end
-    s
 end
 p repeatedStrings('aba', 10) #this outputs 7
 p repeatedStrings('a', 1000000) #this outputs 1000000
